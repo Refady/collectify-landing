e8e3c78a82a7e9c4177212a0558a2f111f5bc271
Patryk Skolimowski <revku@revku.dev>
2025-02-23 17:57:11 +0100

    fix: add id to features section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,7 +3,7 @@ import FeatureItem from "@/components/Feature.tsx";
 
 const Features = () => {
   return (
-    <div className="flex flex-col gap-[50px] my-[150px]">
+    <div className="flex flex-col gap-[50px] my-[150px]" id="features">
       {
         features.map((feature) => (
           <FeatureItem key={feature.title} feature={feature} />
