f35d5b8d49af32bdd9da57843312c986247aaa3e
Patryk Skolimowski <revku@revku.dev>
2025-02-23 17:42:22 +0100

    feat: add features section

---
+++ src/components/Feature.tsx
@@ -0,0 +1,22 @@
+import type { Feature as FeatureType } from "@/types.ts";
+
+interface Props {
+  feature: FeatureType;
+}
+
+const Feature: React.FC<Props> = ({ feature }) => {
+  return (
+    <div className="flex flex-col md:flex-row md:even:flex-row-reverse feature">
+      <div className="feature__image w-full relative overflow-hidden">
+        <img src={feature.image} alt={feature.title} className="w-full rounded-md" />
+      </div>
+      <div className="z-10 flex flex-col justify-center md:w-2/3 max-md:mt-[-30px]">
+        <div className="w-[60px] h-[60px] bg-darkgray flex items-center justify-center rounded-md">{feature.icon}</div>
+        <h3 className="text-[28px] font-bold my-[10px]">{feature.title}</h3>
+        <p>{feature.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
---
+++ src/components/Features.tsx
@@ -0,0 +1,16 @@
+import { features } from "@/utils/features.tsx";
+import FeatureItem from "@/components/Feature.tsx";
+
+const Features = () => {
+  return (
+    <div className="flex flex-col gap-[50px] my-[150px]">
+      {
+        features.map((feature) => (
+          <FeatureItem key={feature.title} feature={feature} />
+        ))
+      }
+    </div>
+  );
+};
+
+export default Features;
