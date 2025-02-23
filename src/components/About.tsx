e70b650cce87e3b320084babc944c6af7d39e47d
Patryk Skolimowski <revku@revku.dev>
2025-02-23 16:15:57 +0100

    feat: add gradient in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,14 +3,14 @@ import { LogIn } from "lucide-react";
 
 const About = () => {
   return (
-    <div className="my-[100px] flex flex-col items-center about" id="about">
+    <div className="my-[100px] flex flex-col items-center about relative" id="about">
       <h2
-        className="text-[30px] font-bold leading-10 text-center mb-[50px] about__title relative md:text-[50px] md:leading-16 max-w-[800px] md:mb-[100px]">Twój
+        className="text-[35px] font-bold leading-10 text-center mb-[60px] about__title relative md:text-[50px] md:leading-16 max-w-[800px] md:mb-[100px]">Twój
         sposób na
         organizację
         klasowego
         budżetu</h2>
-      <p className="text-[17px] text-center leading-8 md:text-[20px] font-medium max-w-[800px]">
+      <p className="text-[18px] text-center leading-10 md:text-[20px] font-medium max-w-[800px] md:leading-12">
         Ta aplikacja została stworzona z myślą o klasowych skarbnikach, którzy chcą uporządkować proces zbierania
         pieniędzy od wszystkich uczniów.
         <br /><br />
@@ -18,7 +18,9 @@ const About = () => {
         śledzenie wpłat. Niech zbieranie pieniędzy stanie się prostsze niż kiedykolwiek wcześniej dzięki Collectify!
       </p>
 
-      <Button className="mt-[50px] md:mt-[80px]"><LogIn /> Założ darmowe konto</Button>
+      <a href="/app/">
+        <Button className="mt-[50px] md:mt-[80px]"><LogIn /> Założ darmowe konto</Button>
+      </a>
     </div>
   );
 };
