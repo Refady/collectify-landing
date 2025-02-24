bf820ea8443f4d98e6904ab4d163c751bd167b8f
Patryk Skolimowski <revku@revku.dev>
2025-02-24 22:40:58 +0100

    style: make navbar sticky

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,17 +9,20 @@ interface Props {
 
 const Navigation = ({ navItems }) => {
   return (
-    <div className="py-[30px] flex justify-between items-center">
-      <a href="/">
-        <img src={logo.src} alt="Collectify Logo" className="w-[150px]" />
-      </a>
+    <div className="w-full bg-background sticky top-0 z-30 backdrop-opacity-10">
+      <div
+        className="p-[30px] flex justify-between items-center w-full max-w-[1200px] mx-auto">
+        <a href="/">
+          <img src={logo.src} alt="Collectify Logo" className="w-[150px]" />
+        </a>
 
-      <div className="hidden md:block">
-        <DesktopNav items={navItems} />
-      </div>
+        <div className="hidden md:block z-50">
+          <DesktopNav items={navItems} />
+        </div>
 
-      <div className="md:hidden">
-        <MobileNav items={navItems} />
+        <div className="md:hidden z-50">
+          <MobileNav items={navItems} />
+        </div>
       </div>
     </div>
   );
