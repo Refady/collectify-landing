2cd058e77d96fd2375c9afd23794c0c672efe954
Patryk Skolimowski <revkuu@gmail.com>
2025-09-02 08:23:44 +0200

    feat: add netlify files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify/functions"; // Netlify Functions adapter
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify({}), // Enables SSR on Netlify Functions
+  output: "static",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  output: "server",
-  integrations: [react()]
+  integrations: [react()],
 });
